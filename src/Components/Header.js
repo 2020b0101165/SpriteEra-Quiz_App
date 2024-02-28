bda89acb43d521baa167b75cf2600277198f77b5
Muskan jaiswal <muskan14sannu@gmail.com>
2024-02-28 07:16:42 +0530

    SpriteEra-quizApp

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img src="logo512.png" alt="LOgo" />
+      <h1> Quiz Mode</h1>
+    </header>
+  );
+}
+
+export default Header;
